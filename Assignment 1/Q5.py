54a3d1992d42a3e6941050e9fdf12c3740db573e
bit-yottabyte <60991230+bit-yottabyte@users.noreply.github.com>
2023-10-17 13:16:53 -0400

    Update Q5.py
    Fixed bugs for strings consisting of a majority of numbers. Now handles all cases efficiently.

--- Assignment 1/Q5.py
+++ Assignment 1/Q5.py
@@ -35,35 +35,36 @@ def delta_debugger(input_str, splits):
     sections = [] #contains all string sections
     problem = "" #Keeps strings with problems
     remainder = len(input_str)
-
     
     split = int(len(input_str)/splits)#number of characters in each section
-    if(split == 0):
+
+    if(split == 0): #If our input is smaller than our sections we need to have 1
         splits = len(input_str)
         split = 1
     print(split)
-    for section in range(splits):
-        if section+1 < splits: #to deal with odd sectioning
+    print(f'Splits:{splits}')
+
+    for section in range(splits): # Sections the string for search
+        if section < splits-1: #to deal with odd sectioning
             sections.append(input_str[split*section:split*(section+1)])
-            remainder-=split
         else:
-            print(remainder)
-            sections.append(input_str[remainder:])
+            print(remainder-split*section)
+            sections.append(input_str[remainder-(remainder-split*section):])
     print(sections)
 
-    for section in sections:
-        if processString(section) != correctString(section): # Check Left Half
+    for section in sections: # Processes the sections
+        if processString(section) != correctString(section): # Check Sections
             problem += section
 
-    print(problem)
+    print(f'problem so far: {problem}')
     if problem == "": #This is situation where there is nothing wrong in the input
         return ""
-    elif problem == input_str:
-        return delta_debugger(problem, split+1)
-    elif splits == len(input_str):
+    elif len(problem)*2 == len(processString(problem)): 
         return problem
+    elif problem == input_str:
+        return delta_debugger(problem, splits+1) #If the problem is still the same size as the input then we need to split it further
     else:
-      return delta_debugger(problem, splits)
+      return delta_debugger(problem, splits) #If the problem persists and there was progress then continue the search as normal 
 
 print(f'The issue is: {delta_debugger("abcdefG1", 2)}')
 #strings = {["abcdefG1"], ["CCDDEExy"], ["1234567b"], ["8655"]}
